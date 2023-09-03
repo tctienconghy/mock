8aaf3499bdbe95df0753aac6f740c7908010c4f4
tctienconghy <55757061+tctienconghy@users.noreply.github.com>
2023-09-03 18:40:17 +0700

    Update select_workspace.sh

--- select_workspace.sh
+++ select_workspace.sh
@@ -7,6 +7,8 @@ cd env/$workspace_name
 # Check if the workspace exists
 if ! terraform workspace select $workspace_name 2>/dev/null; then
   # If the workspace doesn't exist, create it and select it
+  echo "Start to choose workspace!"
   terraform workspace new $workspace_name
   terraform workspace select $workspace_name
+  terraform workspace show
 fi
