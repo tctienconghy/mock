ba9bb479e535b997788f8061a4aa475552cf02fb
tctienconghy <55757061+tctienconghy@users.noreply.github.com>
2023-09-05 19:23:20 +0700

    Update compute.tf

--- env/dev/frontend/compute.tf
+++ env/dev/frontend/compute.tf
@@ -2,3 +2,22 @@ module "ec2_instances" {
     source = "../../../module/ec2"
     ec2_instances = var.ec2_instances
 }
+
+
+resource "null_resource" "ansible_inventory" {
+  triggers = {
+    mytrigger = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo '{
+        "ec2": {
+          "hosts": {
+            ${join(",", [for instance in module.ec2_instances.list_ec2_ip : "${instance}"])},
+          }
+        }
+      }' > /path/to/ansible_inventory.json
+    EOT
+  }
+}
