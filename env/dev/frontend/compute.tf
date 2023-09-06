d24b74f9e46be6dcf365edcc1d5da74bf581f495
tctienconghy <55757061+tctienconghy@users.noreply.github.com>
2023-09-07 00:52:42 +0700

    Update compute.tf

--- env/dev/frontend/compute.tf
+++ env/dev/frontend/compute.tf
@@ -11,13 +11,13 @@ resource "null_resource" "ansible_inventory" {
   triggers = {
     myTrigger = timestamp()
   }
-  
+
   count = length(local.list_ec2_ip)
 
   provisioner "local-exec" {
     command = <<-EOF
-      echo "[ec2_hosts]" > dev_dynamic_inventory
-      echo "${local.list_ec2_ip[count.index]} ansible_user=\"ubuntu\" ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" >> dev_dynamic_inventory
+      echo "[ec2_hosts]" > /Users/tctienconghygmail.com/.jenkins/workspace/jenkins_mock/dev_dynamic_inventory
+      echo "${local.list_ec2_ip[count.index]} ansible_user=\"ubuntu\" ansible_ssh_extra_args='-o StrictHostKeyChecking=no'" >> /Users/tctienconghygmail.com/.jenkins/workspace/jenkins_mock/dev_dynamic_inventory
     EOF
   }
   depends_on = [ module.ec2_instances.list_ec2_ip ]
